fcbc5dd6d048ecfe63efb2af8049d06bb81f2158
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-03-14 22:36:58 +0700

    fix: config headers middleware

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,27 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
